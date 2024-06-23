7766450bce5db930a285acd341fc9525b791dcb4
rafaeljuniora <160675357+rafaeljuniora@users.noreply.github.com>
2024-06-23 15:43:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function scrolllor(){
+    var left=document.querySelector(".scroll-images-orquideas");
+    left.scrollBy(750, 0);
+}
+function scrollror(){
+    var right=document.querySelector(".scroll-images-orquideas");
+    right.scrollBy(-750, 0);
+}
